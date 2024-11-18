9d1732979d93b3c568560b8da0528bffec355bbc
s1avo <s.nadolny@hotmail.com>
2024-11-18 20:33:32 +0100

    fix: add overflow-x-auto to realization's tabs

--- src/ui/molecules/OfferRealizationsTabs.tsx
+++ src/ui/molecules/OfferRealizationsTabs.tsx
@@ -3,14 +3,14 @@ import { Tabs, TabsList, TabsTrigger } from "@/ui/shadcn/tabs";
 
 export const OfferRealizationsTabs = ({ tabs }: { tabs: { title: string; slug: string }[] }) => {
 	return (
-		<Tabs defaultValue={tabs[0]?.slug} className="mx-auto mt-8 max-w-screen-xl px-4 sm:px-6 lg:px-8">
-			<TabsList className="mx-auto w-full rounded-none bg-background">
+		<Tabs defaultValue={tabs[0]?.slug} className="mx-auto mt-8 h-12 overflow-x-auto">
+			<TabsList className="flex w-full justify-start gap-4 rounded-none bg-background sm:justify-center">
 				{tabs.map(({ title, slug }) => (
 					<TabsTrigger
 						key={slug}
 						value={slug}
 						asChild
-						className="rounded-b-none border-b-2 border-b-primary/0 py-3 text-base hover:bg-primary-foreground data-[state=active]:border-b-primary/100 data-[state=active]:text-primary data-[state=active]:shadow-none"
+						className="flex-shrink-0 rounded-b-none border-b-2 border-b-primary/0 py-3 text-base hover:bg-primary-foreground data-[state=active]:border-b-primary/100 data-[state=active]:text-primary data-[state=active]:shadow-none"
 					>
 						<Link href={`?realizacje=${slug}`} scroll={false}>
 							{title}
